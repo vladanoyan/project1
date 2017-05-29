cf1198beaa175208000cc66f3bea448501c4cc24
Vlad <anoyanvlad@gmail.com>
2017-05-29 11:55:51 +0400

    first commit

--- js/js.js
+++ js/js.js
@@ -1,6 +1,15 @@
 
 /*Carousel Head*/
 $(document).ready(function(){
+
+
+
+
+    /** Refresh Modal Window Content */
+    $(document).on('hidden.bs.modal', function (e) {
+        $(e.target).removeData('bs.modal');
+    });
+
 /*__________Tooltip Initialization Starts__________*/
 var startTooltip= function() {
     var hideAllTooltips = function() {
@@ -61,22 +70,303 @@ $(".navbar-toggle").click(function(){
 
     /*_________ Form Wizard Model1 Starts __________*/
     /* Form Validate Function */
-    var validateForm = function(tabId, sectionId) {
+    var validateForm = function(tabId, sectionId, tab) {
+
+        if(tab != true){
+            tab = false;
+        }
+
         switch(sectionId)
         {
             case 'property-inf-request':
                 switch(tabId)
                 {
                     case 'tab1':
-                        return true;
-                        break;
-                    case 'tab2':
-                        return false;
-                        break;
-                    case 'tab3':
-                        return true;
+
+                        $.ajax({
+                            type: 'POST',
+                            url: "/rech_agence_code.php",
+                            data: "stepOne=1&client_nom=" + $("#client_nom").val() + "&client_prenom="+$("#client_prenom").val()+"&client_tel="+$("#client_tel").val()+"&client_email2="+$("#client_email2").val()+"&client_cp="+$("#client_cp").val()+"&client_ville="+$("#client_ville").val()+"&referer=" + $('#referer').val() + "&checkdata=" + $('#checkdata').val() + "&method="+$('#method').val(),
+                            dataType:'json',
+                            async:false,
+                            success: function (data){
+
+                                $.each(data, function (k, v) {
+                                    var input = $('[name=' + k + ']');
+                                    var parentInput = input.closest('.form-group');
+
+                                    parentInput.addClass('form-error form-error-m1');
+                                    parentInput.append('<div class="form-error-blck"> ' +
+                                        '<a href="#" title="' + v + '" data-placement="top"  class="error-btn" data-toggle="tooltip">' +
+                                        ' <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></a>' +
+                                        '</div>');
+
+                                    input.change(function () {
+                                        if (input.val().length != null) {
+                                            parentInput.removeClass('form-error');
+                                        } else {
+                                            parentInput.addClass('form-error');
+                                        }
+                                    });
+                                });
+                                if(data.length == 0)
+                                    tab=true;
+                                else
+                                    tab=false;
+                            },
+
+
+                        });
+
+
+                        return tab;
                         break;
 
+                    case 'tab2':
+
+                        $.ajax({
+                            type: 'POST',
+                            url: "/rech_agence_code.php",
+                            data: "stepOne=2&TypeAnnonce=" + $("#TypeAnnonce").val() + "&TypeBien1=" + $("#TypeBien1").val() + "&Departement=" + $("#Departement").val() + "&Secteur=" + $("#Secteur").val() + "&secteur_insee=" + $("#secteur_insee").val() + "&secteur_nom=" + $("#secteur_nom").val() + "&show_name=" + $("#show_name").val() + "&client_email2=" + $("#client_email2").val() + "&client_cp=" + $("#client_cp").val() + "&client_ville=" + $("#client_ville").val() + "&referer=" + $('#referer').val() + "&checkdata=" + $('#checkdata').val() + "&method=" + $('#method').val(),
+                            dataType: 'json',
+                            async: false,
+                            success: function (data) {
+
+                                $.each(data, function (k, v) {
+                                    var input = $('[name=' + k + ']');
+                                    var parentInput = input.closest('.form-group');
+
+                                    parentInput.addClass('form-error form-error-m1');
+                                    parentInput.append('<div class="form-error-blck"> ' +
+                                        '<a href="#" title="' + v + '" data-placement="top"  class="error-btn" data-toggle="tooltip">' +
+                                        ' <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></a>' +
+                                        '</div>');
+
+                                    input.change(function () {
+                                        if (input.val().length != null) {
+                                            parentInput.removeClass('form-error');
+                                        } else {
+                                            parentInput.addClass('form-error');
+                                        }
+                                    });
+                                });
+                                if (data.length == 0)
+                                    tab = true;
+                                else
+                                    tab = false;
+                            },
+
+                        });
+
+                        return tab;
+                        break;
+
+
+                    case 'tab3':
+
+                        $.ajax({
+                            type: 'POST',
+                            url: "/rech_agence_code.php",
+                            data: "stepOne=3",
+                            dataType:'json',
+                            async:false,
+                            success: function (data){
+
+                                $.each(data, function (k, v) {
+                                    var input = $('[name=' + k + ']');
+                                    var parentInput = input.closest('.form-group');
+
+                                    parentInput.addClass('form-error form-error-m1');
+                                    parentInput.append('<div class="form-error-blck"> ' +
+                                        '<a href="#" title="' + v + '" data-placement="top"  class="error-btn" data-toggle="tooltip">' +
+                                        ' <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></a>' +
+                                        '</div>');
+
+                                    input.change(function () {
+                                        if (input.val().length != null) {
+                                            parentInput.removeClass('form-error');
+                                        } else {
+                                            parentInput.addClass('form-error');
+                                        }
+                                    });
+                                });
+                                if(data.length == 0)
+                                    tab=true;
+                                else
+                                    tab=true;
+                            },
+
+                        });
+                        return tab;
+                        break;
+                }
+
+            case 'property-inf-submit':
+                switch(tabId)
+                {
+                    case 'tab1':
+
+                        $.ajax({
+                            type: 'POST',
+                            url: "/nvannonce_agence_code.php",
+                            data: "stepOne=1&client_nom=" + $("#client_nom").val() + "&client_prenom="+$("#client_prenom").val()+"&client_tel1="+$("#client_tel1").val()+"&client_email="+$("#client_email").val()+"&client_cp="+$("#client_cp").val()+"&client_ville="+$("#client_ville").val()+"&fiche_secteur_id=" + $('#fiche_secteur_id').val() + "&fiche_departement_id="+$("#fiche_departement_id").val()+"&checkdata=" + $('#checkdata').val() + "&method="+$('#method').val(),
+                            dataType:'json',
+                            async:false,
+                            success: function (data){
+
+                                $.each(data, function (k, v) {
+                                    var input = $('[name=' + k + ']');
+                                    var parentInput = input.closest('.form-group');
+
+                                    parentInput.addClass('form-error form-error-m1');
+                                    parentInput.append('<div class="form-error-blck"> ' +
+                                        '<a href="#" title="' + v + '" data-placement="top"  class="error-btn" data-toggle="tooltip">' +
+                                        ' <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></a>' +
+                                        '</div>');
+
+                                    input.change(function () {
+                                        if (input.val().length != null) {
+                                            parentInput.removeClass('form-error');
+                                        } else {
+                                            parentInput.addClass('form-error');
+                                        }
+                                    });
+                                });
+                                if(data.length == 0)
+                                    tab=true;
+                                else
+                                    tab=false;
+                            },
+
+                        });
+                        return tab;
+                        break;
+                    case 'tab2':
+
+                        $.ajax({
+                            type: 'POST',
+                            url: "/nvannonce_agence_code.php",
+                            data: "stepOne=2&fiche_bien_id=" + $("#fiche_bien_id").val() + "&fiche_cp="+$("#fiche_cp").val()+"&fiche_type_id="+$("#fiche_type_id").val()+"&Departement="+$("#Departement").val()+"&Secteur="+$("#Secteur").val()+"&secteur_insee="+$("#secteur_insee").val()+"&secteur_nom="+$("#secteur_nom").val()+"&show_name="+$("#show_name").val()+"&client_email2="+$("#client_email2").val()+"&client_cp="+$("#client_cp").val()+"&client_ville="+$("#client_ville").val()+"&referer=" + $('#referer').val() + "&checkdata=" + $('#checkdata').val() + "&method="+$('#method').val(),
+                            dataType:'json',
+                            async:false,
+                            success: function (data){
+
+                                $.each(data, function (k, v) {
+                                    var input = $('[name=' + k + ']');
+                                    var parentInput = input.closest('.form-group');
+
+                                    parentInput.addClass('form-error form-error-m1');
+                                    parentInput.append('<div class="form-error-blck"> ' +
+                                        '<a href="#" title="' + v + '" data-placement="top"  class="error-btn" data-toggle="tooltip">' +
+                                        ' <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></a>' +
+                                        '</div>');
+
+                                    input.change(function () {
+                                        if (input.val().length != null) {
+                                            parentInput.removeClass('form-error');
+                                        } else {
+                                            parentInput.addClass('form-error');
+                                        }
+                                    });
+                                });
+                                if(data.length == 0)
+                                    tab=true;
+                                else
+                                    tab=false;
+                            },
+
+                        });
+
+                        return tab;
+                        break;
+                    case 'tab3':
+                        tab = true;
+                        return tab;
+                        break;
+                }
+            case 'contact':
+
+                switch(tabId)
+                {
+
+                    case 'tab1':
+
+                        $.ajax({
+                            type: 'POST',
+                            url: "/contacter_agence_code.php",
+                            data: "stepOne=1&form_client_nom=" + $("#form_client_nom").val() + "&form_client_prenom="+$("#form_client_prenom").val()+"&form_client_tel1="+$("#form_client_tel1").val()+"&form_client_email="+$("#form_client_email").val()+"&form_client_cp="+$("#form_client_cp").val()+"&form_client_ville="+$("#form_client_ville").val()+"&checkdata=" + $('#checkdata').val(),
+                            dataType:'json',
+                            async:false,
+                            success: function (data){
+
+                                $.each(data, function (k, v) {
+                                    var input = $('[name=' + k + ']');
+                                    var parentInput = input.closest('.form-group');
+
+                                    parentInput.addClass('form-error form-error-m1');
+                                    parentInput.append('<div class="form-error-blck"> ' +
+                                        '<a href="#" title="' + v + '" data-placement="top"  class="error-btn" data-toggle="tooltip">' +
+                                        ' <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></a>' +
+                                        '</div>');
+
+                                    input.change(function () {
+                                        if (input.val().length != null) {
+                                            parentInput.removeClass('form-error');
+                                        } else {
+                                            parentInput.addClass('form-error');
+                                        }
+                                    });
+                                });
+                                if(data.length == 0)
+                                    tab=true;
+                                else
+                                    tab=false;
+                            },
+
+                        });
+                        return tab;
+                        break;
+                    case 'tab2':
+
+                        $.ajax({
+                            type: 'POST',
+                            url: "/contacter_agence_code.php",
+                            data: "stepOne=2&checkdata=" + $('#checkdata').val(),
+                            dataType:'json',
+                            async:false,
+                            success: function (data){
+                                $.each(data, function (k, v) {
+                                    var input = $('[name=' + k + ']');
+                                    var parentInput = input.closest('.form-group');
+
+                                    parentInput.addClass('form-error form-error-m1');
+                                    parentInput.append('<div class="form-error-blck"> ' +
+                                        '<a href="#" title="' + v + '" data-placement="top"  class="error-btn" data-toggle="tooltip">' +
+                                        ' <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></a>' +
+                                        '</div>');
+
+                                    input.change(function () {
+                                        if (input.val().length != null) {
+                                            parentInput.removeClass('form-error');
+                                        } else {
+                                            parentInput.addClass('form-error');
+                                        }
+                                    });
+                                });
+                                if(data.length == 0)
+                                    tab=true;
+                                else
+                                    tab=false;
+                            },
+
+                        });
+
+                        return tab;
+                        break;
+                    case 'tab3':
+                        tab = true;
+                        return tab;
+                        break;
                 }
         }
         return true;
@@ -92,39 +382,8 @@ $(".navbar-toggle").click(function(){
 
         /*$('#form-wizard').find('li').removeClass('visited-step');*/
 
-        /*__________File Input Elipsis Functions Start__________*/
-        var setFileInputTextElipsis=function(cur) {
-            var filename = $(cur).val().split('\\').pop();
-            if(filename=='')
-                filename=$(cur).attr('data-placeholder');
-            var allowableWidth=$(cur).parent().find(".file-custom").outerWidth()-$(cur).parent().find( ".file-custom-txt" ).outerWidth();
-            $(cur).parent().find( ".file-name-inner" ).text(middleElipsis(filename,allowableWidth));
+    
 
-            $(cur).parent().find( ".file-name").outerWidth($(cur).parent().outerWidth()-$(cur).parent().find( ".file-custom-txt").outerWidth());
-            if($('.file-custom-left').length)
-                $(cur).parent().find( ".file-custom-left" ).css('padding-left',$(cur).parent().find( ".file-custom-txt").outerWidth());
-            if($('.file-custom-right').length)
-                $(cur).parent().find( ".file-custom-right" ).css('padding-right',$(cur).parent().find( ".file-custom-txt").outerWidth());
-        };
-
-        $('.file-input').change(function() {
-            setFileInputTextElipsis(this);
-        });
-
-        $(function(){
-            $('.file-input').each(function(index, element) {
-                setFileInputTextElipsis(this);
-            });
-        });
-
-        $(window).resize(function(){
-            $('.file-input').each(function(index, element) {
-                setFileInputTextElipsis(this);
-            });
-        });
-
-
-        /*__________File Input Elipsis Functions End__________*/
         /* Validation for Current Tab */
         var curTabHref=tabEl.find('>a').attr('href');
         var curTabId=curTabHref.substring(1, curTabHref.length);
@@ -225,10 +484,13 @@ $('#nav-icon3').click(function(){
     });
 /*chenge btn text*/
 
-        $('.SeeMore2').click(function(){
-        $('#equipment').toggleClass('flashOn flash')
-    });
+     
+    $("#show").hide();
+    $(".SeeMore2").click(function(){
+        $("#hide").toggle();
+        $("#show").toggle();
 
+    });
    /*__________Listing Model3 Functions Start_________*/
 /* Match Item heights for Listing Item */
 var matchHeightLstItm=function(){
@@ -304,4 +566,11 @@ $(function(){
     });
     /*__________DPE GES Hover Effect Ends________*/
 
+
+
+
 });
+
+
+
+
